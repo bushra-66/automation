85f76e189707fd6ab768b881d34d14d3d67b6993
bushra-99 <73484459+bushra-99@users.noreply.github.com>
2023-09-21 08:34:37 +0300

    update forget continue

--- cypress/e2e/automation.cy.js
+++ cypress/e2e/automation.cy.js
@@ -3,15 +3,20 @@ const expectedCurruncy='SAR'
 const expectedAttrubute='lang'
 const expextedLang= 'en'
 describe('Aspire test', () => {
-  it('test the currency is SAR', () => {
+  it.skip('test the currency is SAR', () => {
     cy.visit('https://www.almosafer.com/en')
     cy.get('[data-testid="Header__CurrencySelector"]').invoke('text')
     .should('include',expectedCurruncy)
  })
- it('test the expected language', () => {
+ it.skip('test the expected language', () => {
   cy.visit('https://www.almosafer.com/en')
   cy.get('html').should('have.attr',expectedAttrubute,expextedLang)
 
   
  })
+ it('check language randomely', () => {
+  let websites=["https://www.almosafer.com/en","https://www.almosafer.com/ar"]
+  let randomIndex= Math.floor(Math.random()*websites.length)
+  cy.visit(websites[randomIndex])
+ });
 })
